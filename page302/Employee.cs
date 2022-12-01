013ff705ea436591d1ad7dd8c9875a87cd582f3c
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-11-30 22:56:02 -0800

    Add project files.

---
+++ page302/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page302
+{
+    public class Employee : Person
+    {
+        public override void FirstName()
+        {
+            Console.WriteLine(SayName);
+            Console.ReadLine();
+        }
+    }
+
+    {
+
+    }
+}
---
+++ page302/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page302
+{
+    abstract class Person
+    {
+        public abstract void  FirstName();
+
+        public abstract void LastName();
+
+        int SayName = 
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
