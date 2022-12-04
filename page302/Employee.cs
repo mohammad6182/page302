01dc9dfefaff00f23948ebcecd5e6c7ac87f3eb8
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-12-04 14:38:01 -0800

    add

--- page302/Employee.cs
+++ page302/Employee.cs
@@ -6,13 +6,18 @@ using System.Threading.Tasks;
 
 namespace page302
 {
-    class Employee : Person
+    class Employee : Person, IQuittable
     {
         public override void FirstName()
         {
             Console.WriteLine("SayNAme is : ");
             Console.ReadLine();
         }
+        public void Quit(Employee employee)
+        {
+
+        }
+
     }
 
     
